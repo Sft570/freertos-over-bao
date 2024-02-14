7978dcc48475ff79d0659e6d10d096b6b4ecf4b6
Sft570 <sft570@alumnos.unican.es>
2024-02-14 15:58:26 +0100

    feat: add test task

--- src/main.c
+++ src/main.c
@@ -52,7 +52,7 @@ void vTask(void *pvParameters)
     unsigned long id = (unsigned long)pvParameters;
     while (1)
     {
-        printf("Task%d: %d\n", id, counter++); 
+        printf("TEST TEST Task%d: %d\n", id, counter++); 
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
